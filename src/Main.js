4895b266834347b713d7e2de140e121ab34c136f
Kieran Woolfe <kwoolfe@gmail.com>
2024-04-14 15:55:28 +0100

    Added hero section and converted css to scss

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,38 @@
+import food_hero from './Assets/restauranfood.jpg'
+
 function Main() {
     return (
         <main>
-
+            <CallToAction/>
         </main>
     );
 }
+
+function CallToAction() {
+    return (
+        <section id="hero-section">
+        <div class="row" id="hero-row">
+            <div class="row-content" id="hero-row-content">
+                <article id="hero-content-left">
+                    <h1 id="hero-title">Little Lemon</h1>
+                        <h2 id="hero-subtitle">Chicago</h2>
+                        <p>
+                            We are a family owned Mediterranean restaurant,
+                            focused on traditional recipes served with a modern twist.
+                        </p>
+                    <button type="button">Reserve a table</button>
+                </article>
+
+                <img
+                    src={food_hero}
+                    alt="A server carrying a tracy of delicious food"
+                    width={417}
+                />
+            </div>
+        </div>
+        </section>
+    )
+}
+
 
 export default Main;
