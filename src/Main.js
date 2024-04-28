5c568f50e76a6bad59d32b9a608fc8ea626b303b
Kieran Woolfe <kwoolfe@gmail.com>
2024-04-28 08:47:06 +0100

    Added specials and testimonials

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,14 @@
 import food_hero from './Assets/restauranfood.jpg'
+import specials from "./Assets/specials.json"
+import testimonials from "./Assets/testimonials.json"
+import star from "./Assets/star.png"
 
 function Main() {
     return (
         <main>
             <CallToAction/>
+            <Specials data={specials}/>
+            <CustomersSay data={testimonials}/>
         </main>
     );
 }
@@ -34,5 +39,82 @@ function CallToAction() {
     )
 }
 
+function Specials (props) {
+    return (
+        <section class="row" id="specials-row">
+            <div class="row-content" id="specials-row-content">
+                <h1>This week's specials!</h1>
+                <div id="specials-section">
+                    {props.data.map(
+                        (special) => SpecialCard(special)
+                    )}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function SpecialCard (item) {
+    return (
+        <article class="special" id={item.name}>
+            <img
+                src={require(`./Assets/${item.image}`)}
+                alt="Food available to order"
+                width="100px"
+            />
+            <div class="card-body">
+                <div class="card-header">
+                    <h4 class="card-title">{item.name}</h4>
+                    <p class="card-price">
+                        <span class="highlight">{item.price}</span>
+                    </p>
+                </div>
+                <p class="card-description">{item.description}</p>
+                <a>Order online</a>
+            </div>
+        </article>
+    )
+}
+
+function CustomersSay (props) {
+    return (
+        <section class="row" id="customers-say-row">
+            <div class="row-content" id="customers-say-row-content">
+                <h1>Testimonials</h1>
+                <div id="testimonials-section">
+                    {props.data.map(
+                        (testimonial) => CustomerCard(testimonial)
+                    )}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function CustomerCard (item) {
+    return (
+        <article class="testimonial" id={item.name}>
+            <div class="testimonial-body">
+                <h4 class="testimonial-title">{item.name}</h4>
+                <img 
+                    src={require(`./Assets/${item.picture}`)}
+                    alt="Profile picture"
+                    width="50px"
+                    class="testimonial-profile"
+                />
+                <p>{item.review}</p>
+                <div class="testimonial-stars">
+                    {new Array(item.rating).fill('').map(
+                        (_, index) => <img 
+                            src = {star}
+                            id = {index}
+                            class = "review-star"
+                        />
+                    )}
+                </div>
+            </div>
+        </article>
+    )
+}
 
 export default Main;
