b5be014792bac58a767c27a01097a6e9a6a6a392
Kieran Woolfe <kwoolfe@gmail.com>
2024-05-04 20:05:13 +0100

    Added booking form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import BookingPage from './Components/Booking'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Main/>}/>
+        <Route path="reservations" element={<BookingPage/>}/>
       </Routes>
     <Footer/>
     </BrowserRouter>
