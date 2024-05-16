16c41e04ea7452c5c45b5e3275e712e5bbe9bc2f
Kieran Woolfe <kwoolfe@gmail.com>
2024-05-16 22:56:02 +0100

    Tweaks to responsive design on homepage

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import logo from '../Assets/grey_logo.png'
 import { Link } from "react-router-dom";
 
 function Footer () {
+
     return (
         <footer>
         <div className="row" id="row-footer">
@@ -12,34 +13,36 @@ function Footer () {
                 alt = "A chef preparing food"
                 height = {300}
             />
-            <div className="footer-nav-box">
-                <h3>Navigation</h3>
-                <nav>
-                    <ul>
-                        <li><Link to="/Home">Home</Link></li>
-                        <li><Link to="/About">About</Link></li>
-                        <li><Link to="/Menu">Menu</Link></li>
-                        <li><Link to="/Reservations">Reservations</Link></li>
-                        <li><Link to="/Order">Order online</Link></li>
-                        <li><Link to="/Login">Login</Link></li>
+            <div id = "footer-right">
+                <div className="footer-nav-box">
+                    <h3>Navigation</h3>
+                    <nav>
+                        <ul>
+                            <li><Link to="/Home">Home</Link></li>
+                            <li><Link to="/About">About</Link></li>
+                            <li><Link to="/Menu">Menu</Link></li>
+                            <li><Link to="/Reservations">Reservations</Link></li>
+                            <li><Link to="/Order">Order online</Link></li>
+                            <li><Link to="/Login">Login</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="footer-nav-box">
+                    <h3>Contact</h3>
+                    <ul id="footer-contact">
+                        <li>123 Food Street,<br/>Chicago</li>
+                        <li>(00) 4321 4567 21</li>
+                        <li>hello@littlelemon.com</li>
                     </ul>
-                </nav>
-            </div>
-            <div className="footer-nav-box">
-                <h3>Contact</h3>
-                <ul id="footer-contact">
-                    <li>123 Food Street, Chicago</li>
-                    <li>(00) 4321 4567 21</li>
-                    <li>hello@littlelemon.com</li>
-                </ul>
-            </div>
-            <div className="footer-nav-box">
-                <h3>Social media</h3>
-                <nav><ul>
-                    <li><a href = "//www.facebook.com">Facebook</a></li>
-                    <li><a href = "//www.instagram.com">Instagram</a></li>
-                    <li><a href = "//twitter.com">Twitter</a></li>
-                </ul></nav>
+                </div>
+                <div className="footer-nav-box">
+                    <h3>Social media</h3>
+                    <nav><ul>
+                        <li><a href = "//www.facebook.com">Facebook</a></li>
+                        <li><a href = "//www.instagram.com">Instagram</a></li>
+                        <li><a href = "//twitter.com">Twitter</a></li>
+                    </ul></nav>
+                </div>
             </div>
         </div>
         </div>
